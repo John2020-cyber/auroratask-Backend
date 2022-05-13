0a46b9519765ce9af32e69e40853061e12b7ea2f
Zain-DevOps <siddiqui.zain1@gmail.com>
2022-05-13 23:58:27 +0500

    backend developed in nodejs-express with port 3000

---
+++ backend.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const name = process.env.NAME || 'Skatater';
+
+app.get('/', (req,res) =>{
+
+    res.send(`Hello ${name}`);
+});
+
+
+
+app.listen(3000, () => console.log('Listening on port 3000..'));
