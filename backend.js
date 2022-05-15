673b8eed35f9d156da2b6590b73258e3edd7d2c8
Zain-DevOps <siddiqui.zain1@gmail.com>
2022-05-15 16:56:00 +0500

    Changed port to 8080 for backend

--- backend.js
+++ backend.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const name = process.env.NAME || 'Stakater';
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 
 app.get('/', (req,res) =>{
 
